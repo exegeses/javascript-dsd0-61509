0a3aa2239d36e0a9fa19a2406561258a6bce8661
Marcos <exegeses@gmail.com>
2022-11-23 10:04:59 -0300

    Teoría arrays

---
+++ clase7/js/arrays.js
@@ -0,0 +1,10 @@
+
+let marca = [
+            'Hermes','Zara','Boss',
+            'Aeropostale','Hollister','Tommy'
+            ];
+
+console.log(marca);
+console.log(marca[0]);
+console.log(marca[1]);
+console.log(marca[2]);
